c94d8460b8beb30ab4996e8140b82daf093af8a3
Kumudu Gunasekara <kumudug1@gmail.com>
2017-09-15 17:08:26 +1000

    memory leak

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿var theThing = null;
+var replaceThing = function () {
+  var originalThing = theThing;
+  var unused = function () {
+    if (originalThing) // a reference to 'originalThing'
+      console.log("hi");
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),
+    someMethod: function () {
+      console.log("message");
+    }
+  };
+};
+setInterval(replaceThing, 1000);
